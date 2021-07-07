74296b3ed03c2b378a1aea276ae31fefa0afed96
EverydayLearner254 <akamuhu442@gmail.com>
2021-07-07 14:01:41 +0300

    changedscript

--- screenshot_master/screenshot/views.py
+++ screenshot_master/screenshot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from desktopmagic.screengrab_win32 import getScreenAsImage
+# from desktopmagic.screengrab_win32 import getScreenAsImage
 import getpass
+import pyscreenshot
+
 
 # Create your views here.
 def index(request):
@@ -11,8 +13,12 @@ def index(request):
 def screenshot(request):
     user = getpass.getuser()
     
-    entireScreen = getScreenAsImage()
-    entireScreen.save(user + r'_screen.png', format='png')
+    # entireScreen = getScreenAsImage()
+    # entireScreen.save(user + r'_screen.png', format='png')
+    
+    image = pyscreenshot.grab()    
+    image.save(user + r"_test.png")
+    
     context = {}
     
     return render(request, 'index.html', context)
